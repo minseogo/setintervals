346929f2401245c53cacc382d7db3f73f644f652
minseogo <gominseo1112@gmail.com>
2024-06-26 11:05:42 +0900

    setinterval 이름이 공통적이지 않을때의 식

--- setinterval.js
+++ setinterval.js
@@ -1,12 +1,24 @@
 // 자바스크립트 경로는 css 다르다 연결된 html 기준으로 처리해야한다.
 
-let count  = 0;
-setInterval(function(){
-    count++;
-    count %= 5;
-    document.querySelector("#bannerimg").setAttribute("src",`./banner/${count}.png`)
-}, 1000);
-
-
+// let count  = 0;
+// setInterval(function(){
+//     count++;
+//     count %= 5;
+//     document.querySelector("#bannerimg").setAttribute("src",`./banner/${count}.png`)
+// }, 1000);
 
 //$("#bannerimg").attr("src","/banner/1.png")
+
+const bannerdata = [
+    "./banner2/1.png",
+    "./banner/0.png",
+    "./banner2/membership.png"
+];
+
+let num = 0;
+document.querySelector("#bannerimg").setAttribute("src",bannerdata[num])
+setInterval(function(){
+    num++;
+    num %= bannerdata.length;
+    document.querySelector("#bannerimg").setAttribute("src",bannerdata[num]) 
+}, 1000)
