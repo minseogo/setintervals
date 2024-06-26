d4dcda6ab96e096964325498804488e4a43e0863
minseogo <gominseo1112@gmail.com>
2024-06-26 12:06:34 +0900

    setinterval play, stop 버튼 추가와 최적화 식

--- setinterval.js
+++ setinterval.js
@@ -8,7 +8,10 @@
 // }, 1000);
 
 //$("#bannerimg").attr("src","/banner/1.png")
+// ------------------------------------------------------------------------------------------------ //
 
+
+// play, stop 버튼 추가
 const bannerdata = [
     "./banner2/1.png",
     "./banner/0.png",
@@ -17,8 +20,56 @@ const bannerdata = [
 
 let num = 0;
 document.querySelector("#bannerimg").setAttribute("src",bannerdata[num])
-setInterval(function(){
+
+document.querySelector(".bannerwrap").style.background = `url(${bannerdata[num]}) center no-repeat`;
+
+    const autobanner = setInterval(function(){
     num++;
     num %= bannerdata.length;
+    document.querySelector(".bannerwrap").style.background = `url(${bannerdata[num]}) center no-repeat`;
     document.querySelector("#bannerimg").setAttribute("src",bannerdata[num]) 
-}, 1000)
+}, 3000)
+
+
+document.querySelector(".bannerwrap button").addEventListener('click', function(){
+    clearInterval(autobanner);
+    this.innerHTML = "Play"
+});
+
+// ---------------------------------------------------------------------------------------- //
+
+
+// setinterval 최적화 버전
+
+const bannerData = [
+    "/banner2/1.png",
+    "/banner/0.png",
+    "/banner2/membership.png"
+];
+
+let num = 0;
+
+const bannerWrap = document.querySelector(".bannerwrap");
+const bannerImg = document.querySelector("#bannerimg");
+const bannerButton = document.querySelector(".bannerwrap button");
+
+function updateBanner() {
+    bannerWrap.style.background = `url(${bannerData[num]}) center no-repeat`;
+    bannerImg.setAttribute("src", bannerData[num]);
+}
+
+function startAutoBanner() {
+    return setInterval(function() {
+        num = (num + 1) % bannerData.length;
+        updateBanner();
+    }, 3000);
+}
+
+let autoBanner = startAutoBanner();
+
+bannerButton.addEventListener('click', function() {
+    clearInterval(autoBanner);
+    bannerButton.innerHTML = "PLAY";
+});
+
+updateBanner(); // 초기 배너 설정
